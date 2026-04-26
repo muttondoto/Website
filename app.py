6c5ede72524b09da8ff71b8d46570ee9067803c9
Einar Cabrera <einn.o@yahoo.com>
2026-04-26 00:45:16 -0600

    Downloads

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import send_from_directory
 
 app = Flask(__name__)
 
@@ -14,3 +15,7 @@ def index_bere():
 @app.route("/Postulado")
 def index_postulado():
         return render_template('Postulado.html')
+
+@app.route('/downloads/<name>')
+def download_file(name):
+        return send_from_directory(app.config["./original"], name)
